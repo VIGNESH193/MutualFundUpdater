a8b19085ed9d65afdb0a6db591c4e078c0ae8ad2
Vignesh Lakshminarayanan <vignesh3052@gmail.com>
2025-05-22 08:43:03 +0530

    Update to tracked funds

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This script fetches the latest NAV values of tracked mutual funds from the AMFI website and updates them in Firestore.
 import firebase_admin
 from firebase_admin import credentials, firestore
 import pandas as pd
@@ -9,11 +10,20 @@ cred = credentials.Certificate("service-account.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+def fetch_tracked_funds():
+    """Fetch tracked funds from Firestore."""
+    tracked_fund_data = db.collection("mutual_funds").document("tracked_funds").get().to_dict()
+    tracked_funds = tracked_fund_data["funds"]
+    
+    return tracked_funds
+
 def fetch_latest_fund_values():
     """Fetch the latest NAVs from AMFI website."""
     url = "https://www.amfiindia.com/spages/NAVAll.txt"
     response = requests.get(url)
     nav_data = response.text.splitlines()
+
+    tracked_funds = fetch_tracked_funds()
 
     data = []
     for line in nav_data[1:]:
@@ -26,7 +36,8 @@ def fetch_latest_fund_values():
         nav_date = parts[5].strip()
         nav_date = pd.to_datetime(nav_date, dayfirst=True).strftime('%d-%m-%Y')
 
-        data.append([scheme_code, scheme_name, nav_value, nav_date])
+        if scheme_code in tracked_funds:
+            data.append([scheme_code, scheme_name, nav_value, nav_date])
 
     df = pd.DataFrame(data, columns=["Scheme Code", "Scheme Name", "NAV Value", "NAV Date"])
 
@@ -37,12 +48,12 @@ def update_firestore():
     latest_values = fetch_latest_fund_values()
     for idx, row in tqdm(latest_values.iterrows(), total=len(latest_values)):
         scheme_code = row["Scheme Code"]
-        doc_ref = db.collection("mf_amfiindia").document(scheme_code)
+        doc_ref = db.collection("mf_live_price").document(scheme_code)
         doc = doc_ref.get()
         if not doc.exists:
             doc_ref.set({
                 "Scheme Code": scheme_code,
-                "Scheme Name": row["Scheme Name"],
+                "Scheme Name": row["Scheme Name"]
             })
         date_ref = doc_ref.collection("history").document(row["NAV Date"])
         date_ref.set({
@@ -51,3 +62,4 @@ def update_firestore():
 
 if __name__ == "__main__":
     update_firestore()
+    print("Firestore updated with latest NAV values.")
